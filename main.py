c02deb97b5117d6f4a9afdeea022cb8fcf563961
Taner Süzen <taner.suzen95@gmail.com>
2023-11-25 13:18:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("white")
+drawing_board.title("Drawing Board")
+
+turtle_instance = turtle.Turtle()
+
+def turtle_forward():
+    turtle_instance.forward(100)
+
+def angle_right():
+    turtle_instance.right(10)
+
+def angle_left():
+    turtle_instance.left(10)
+
+def clear_screen():
+    turtle_instance.clear()
+
+def return_home():
+    turtle_instance.home()
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_forward,key="space")
+drawing_board.onkey(fun=angle_left,key="Up")
+drawing_board.onkey(fun=angle_right,key="Down")
+drawing_board.onkey(fun=clear_screen,key="c")
+drawing_board.onkey(fun=return_home,key="h")
+
+turtle.mainloop()
